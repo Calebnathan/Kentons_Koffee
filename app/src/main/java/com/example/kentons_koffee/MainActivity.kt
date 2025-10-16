655a1e0b45e039159421aac18b6c372d4c3aa5f6
Caleb <calebtynenathan@gmail.com>
2025-10-16 23:05:26 +0200

    Added the necessary navigation layout code

--- app/src/main/java/com/example/kentons_koffee/MainActivity.kt
+++ app/src/main/java/com/example/kentons_koffee/MainActivity.kt
@@ -11,11 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        binding.openDrawerButton.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            //test
+
         }
+
     }
 }
